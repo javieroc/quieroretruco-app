9ec87419162ec78e8ba576b1d6584f880189154f
Javier Ocampo <ferroxido@gmail.com>
2025-02-13 20:08:08 +0000

    setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,48 @@
+import tseslint from 'typescript-eslint'
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      importPlugin.flatConfigs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      prettier
+    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      }
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'no-console': 'warn',
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
 )
